122ecff5e11214f33c9f7250b1d459ee91f0848d
Ramofvm <ramonfhelipe547@gmail.com>
2025-01-29 17:41:04 -0300

    adição dos endpoint e serviços

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+
+  @Post()
+  async CreateUser(@Body() body:{name:string, email:string, password:string}){
+
+    return await this.userService.CreateUser(body)
+     
+  }
+
+  @Delete()
+  async deleteUser(@Body() body: { email: string }) {
+    return await this.userService.DeleteUser(body.email); 
+  }
+
+  @Post('validar')
+
+   async ValidateUser(@Body() body:{email:string, password:string }){
+    return await this.userService.ValidationLogin(body.email, body.password)
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import { throwError } from 'rxjs';
 import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
@@ -8,10 +9,98 @@ export class UserService {
     constructor(private readonly prisma:PrismaService) {}
 
 
-    async CreateUser(data: Prisma.UsersCreateInput){
+    async CreateUser(data: Prisma.UsersCreateInput) {
+        try {
+            const userExists = await this.prisma.users.findUnique({
+                where: {
+                    email: data.email
+                }
+            });
+    
 
-        return this.prisma.users.create({data})
+            if (userExists) {
+                throw {
+                    statusCode: 400,
+                    message: 'Já existe um usuário com esse email',
+                };
+            }
+            await this.prisma.users.create({ data });
+            return {
+                statusCode: 200,
+                message: 'Usuário criado com sucesso',
+            };
+    
+        } catch (error) {
+           
+            throw {
+                statusCode: error.statusCode || 500,
+                message: error.message || 'Não foi possível criar o usuário',
+                originalError: error.originalError || error.message,
+            };
+        }
+    }
+        
 
 
+   
+
+    async DeleteUser(email: string) {
+        try {
+            const user = await this.prisma.users.findUnique({
+                where: {
+                    email: email
+                }
+            });
+    
+            if (!user) {
+                throw{
+                    statusCode:400, message: 'usuario não encontrado'
+                }
+            }
+
+            await this.prisma.users.delete({
+                where: {
+                    email: email
+                }
+
+            
+             });
+
+             return{
+                statusCode:200,
+                message:'Usuario deletado com sucesso'
+             }
+        } catch (error) {
+            throw{
+                statusCode: 500, 
+                message: 'Não foi possível deletar o usuario', 
+                originalError: error.message 
+
+            }
+           
+        }
+    }
+    async ValidationLogin(email: string, password: string) {
+        try {
+            const user = await this.prisma.users.findUnique({
+                where: {
+                    email: email,
+                    password: password
+                }
+            });
+    
+            if (!user) {
+                throw { statusCode: 404, message: 'Usuário não encontrado' };
+            } else {
+                return user;
+            }
+    
+        } catch (error) {
+            throw { 
+                statusCode: 500, 
+                message: 'Não foi possível achar o usuário', 
+                originalError: error.message 
+            };
+        }
     }
 }
